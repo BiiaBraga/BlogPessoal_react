3831e02bdd150d28ba0796fb5ebdbe146e199787
BiiaBraga <beatrizsagrado9@gmail.com>
2026-05-06 14:20:45 -0300

    feat: navbar e footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            bg-indigo-900 text-white'>
+
+                <div className="container flex justify-between text-lg mx-8">
+                    Blog Pessoal
+
+                    <div className='flex gap-4'>
+                        <div>Postagens</div>
+                        <div>Temas</div>
+                        <div>Cadastrar tema</div>
+                        <div>Perfil</div>
+                        <div>Sair</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
