16f46d462350ddada090dcd68b2cbd623d89b660
BiiaBraga <beatrizsagrado9@gmail.com>
2026-05-06 14:52:15 -0300

    feat: adiciona links de navegação e redes sociais

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,6 +8,7 @@ function Footer() {
         <>
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
+
                     <p className='text-xl font-bold'>
                         Blog Pessoal Generation | Copyright: {data}
                     </p>
@@ -17,9 +18,19 @@ function Footer() {
                     </p>
 
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+
+                        <a href="https://www.linkedin.com/in/beatriz-braga-silva/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+
+                        <a href="https://www.instagram.com/biia.braga" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+
+                        <a href="https://www.facebook.com/biia.braga.16" target="_blank">
+                            <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
+
                     </div>
                 </div>
             </div>
