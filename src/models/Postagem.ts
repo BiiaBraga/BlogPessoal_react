1493580ba59f9a86ef261745e0d48733b59a075a
BiiaBraga <beatrizsagrado9@gmail.com>
2026-05-07 12:50:22 -0300

    feat: service e cadastro

---
+++ src/models/Postagem.ts
@@ -0,0 +1,12 @@
+//importações
+import type Tema from "./Tema";
+import type Usuario from "./Usuario";
+
+export default interface Postagem{
+    id: number;
+    titulo: string;
+    texto: string;
+    data: string;
+    tema?: Tema | null;
+    usuario?: Usuario | null;
+}
---
+++ src/models/Tema.ts
@@ -0,0 +1,8 @@
+//importações
+import type Postagem from "./Postagem";
+
+export default interface Tema {
+    id: number;
+    descricao: string;
+    postagem?: Postagem[] | null;
+}
