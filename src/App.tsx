4b768e6cf3b0b65281bac25cb21b274d23438f89
BiiaBraga <beatrizsagrado9@gmail.com>
2026-05-08 12:20:14 -0300

    feat: AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,27 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
 
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/home" element={<Home />}/>
-            <Route path="/cadastro" element={<Cadastro />}/>
-            <Route path="/" element={<Login />}/>
-          </Routes>
-        </div>
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/home" element={<Home />}/>
+              <Route path="/cadastro" element={<Cadastro />}/>
+              <Route path="/login" element={<Login />}/>
+              <Route path="/" element={<Login />}/>
+            </Routes>
+          </div>
 
-        <Footer />
-      </BrowserRouter>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
