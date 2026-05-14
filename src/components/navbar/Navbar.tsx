440b3d963427e8d3107017e85fd9bb149d322d64
BiiaBraga <beatrizsagrado9@gmail.com>
2026-05-14 10:23:56 -0300

    feat: perfil e ajustes finais

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
@@ -15,24 +15,34 @@ function Navbar() {
         navigate('/')
     }
 
-    return (
-        <>
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
             
                 <div className="container flex justify-between text-lg mx-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-                    <p> Bem vindo {usuario.senha}</p>
 
                     <div className='flex gap-4'>
                         <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
-                        <Link to='/cadastrartemas' className='hover:underline'>Cadastrar tema</Link>
-                        Perfil
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
+
+        )
+    }
+
+    return (
+        <>
+            { component }
         </>
     )
 }
